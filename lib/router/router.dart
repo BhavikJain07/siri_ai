de7dc336a1c8d32b6b14c392e29d7f7a9ea172a5
BhavikJain07 <bhavikjainhp@gmail.com>
2025-03-19 15:13:06 +0530

    completed the app

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,7 +1,8 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
+import 'package:siri_ai/pages/home_page.dart';
 import 'package:siri_ai/pages/splash_screen.dart';
-import 'package:siri_ai/pages/linear_regression.dart';
+import 'package:siri_ai/pages/tasks/linear_regression.dart';
 
 final GoRouter router = GoRouter(
   routes: <GoRoute>[
@@ -11,6 +12,12 @@ final GoRouter router = GoRouter(
         return SplashScreen();
       },
       routes: <GoRoute>[
+        GoRoute(
+          path: "/home",
+          builder: (BuildContext context, GoRouterState state) {
+            return HomePage();
+          },
+        ),
         GoRoute(
           path: "/linear",
           builder: (BuildContext context, GoRouterState state) {
